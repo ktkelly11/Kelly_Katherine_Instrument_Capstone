290d3ae9244a56a19703d9c15e569eddbbe308a3
Katherine Kelly <ktkelly11@gmail.com>
2024-06-23 10:02:12 -0400

    Working on NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // import { useState } from 'react'
 import "./App.css";
+import NavBar from "./components/navBar/NavBar.jsx";
 
 function App() {
   return (
     <>
+      <NavBar />
       <h1>Starting my Capstone front end!</h1>
     </>
   );
---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import "./NavBar.css";
+
+export default function NavBar() {
+  return (
+    <>
+      <nav className="navbar">
+        <button className="Home/Secondhand Instruments">
+          Secondhand Instruments
+        </button>
+        <button className="How it Helps">How it Helps</button>
+      </nav>
+    </>
+  );
+}
