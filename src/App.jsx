dd524d5dd886eb796858c5221fdd92a68f83f64c
Katherine Kelly <ktkelly11@gmail.com>
2024-06-23 11:55:02 -0400

    Routes set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 // import { useState } from 'react'
 import "./App.css";
 // import NavBar from "./components/navBar/NavBar.jsx";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import HelpsPage from "./pages/HelpsPage";
+import CommentsPage from "./pages/CommentsPage";
+import DonationsPage from "./pages/DonationsPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/helps" element={<HelpsPage />} />
+          <Route path="/comments" element={<CommentsPage />} />
+          <Route path="/donations" element={<DonationsPage />} />
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
