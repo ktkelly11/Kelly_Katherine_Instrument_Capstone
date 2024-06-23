9ef0966d93952ba4841549407e6dcfb12323be82
Katherine Kelly <ktkelly11@gmail.com>
2024-06-23 14:42:15 -0400

    Navigation links added to Nav Bar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,13 +1,22 @@
 import "./NavBar.css";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <>
       <nav className="navbar">
-        <button className="home">Secondhand Instruments</button>
-        <button className="how-it-helps">How it Helps</button>
-        <button className="comments">Comments</button>
-        <button className="donations">Donations</button>
+        <Link to={"/home"}>
+          <div className="home">Secondhand Instruments</div>
+        </Link>
+        <Link to={"/helps"}>
+          <div className="how-it-helps">How it Helps</div>
+        </Link>
+        <Link to={"/comments"}>
+          <div className="comments">Comments</div>
+        </Link>
+        <Link to={"/donations"}>
+          <div className="donations">Donations</div>
+        </Link>
       </nav>
     </>
   );
