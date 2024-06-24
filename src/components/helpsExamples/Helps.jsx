aa41fa375549eff702a43aaad5c330c8732af232
Katherine Kelly <ktkelly11@gmail.com>
2024-06-24 11:13:40 -0400

    Added pictures for Helps page

---
+++ src/components/helpsExamples/Helps.jsx
@@ -0,0 +1,11 @@
+import "./Helps.css";
+import music_class from "../../images/howItHelps/music_class.webp";
+import music_class_2 from "../../images/howItHelps/music_class_2.jpg";
+
+export default function () {
+  return (
+    <>
+      <div className="teacher-1"></div>
+    </>
+  );
+}
