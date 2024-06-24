4ed6bca429386cc753110df15d5f3796a28305b5
Katherine Kelly <ktkelly11@gmail.com>
2024-06-24 11:56:15 -0400

    Added the rest of the examples to the Helps page

--- src/components/helpsExamples/HelpsExamples.jsx
+++ src/components/helpsExamples/HelpsExamples.jsx
@@ -11,6 +11,21 @@ export default function HelpExamples() {
         <img src={music_class_2} alt="Mrs. Singh teaching a class" />
         <p>Explanation of why the instruments are important</p>
       </div>
+      <div className="teacher-2">
+        <p>Explanation of why the instruments are important</p>
+        <img src={music_class} alt="Mr. Davis teaching a class" />
+      </div>
+      <div className="teacher-3">
+        <img
+          src={music_class_3}
+          alt="Sam talks about what it means to him to be able to create music with the help of Ms. Johnson"
+        />
+        <p>Explanation of why the instruments are important</p>
+      </div>
+      <div className="teacher-4">
+        <p>Explanation of why the instruments are important</p>
+        <img src={music_class_4} alt="Mr. Davis teaching a class" />
+      </div>
     </>
   );
 }
