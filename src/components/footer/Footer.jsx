eb3814e5ce5c13c93a43fb69f47b0f9c362a683f
Katherine Kelly <ktkelly11@gmail.com>
2024-06-24 08:07:04 -0400

    Footer component made

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,19 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <>
+      <div className="footer">
+        <p>You can follow us here:</p>
+        <div className="buttons">
+          <button className="github">
+            <a href="https://github.com/ktkelly11">Github</a>
+          </button>
+          <button className="linkedin">
+            <a href="www.linkedin.com/in/katherine-kelly11">Linkedin</a>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import NavBar from "../components/navBar/NavBar";
 import HomePageInfo from "../components/homePageInfo/HomePageInfo";
 import HomePageIntro from "../components/homePageIntro/HomePageIntro";
+import Footer from "../components/footer/Footer";
 
 function Homepage() {
   return (
