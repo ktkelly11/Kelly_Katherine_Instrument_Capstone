6a2202bcbe2fd76db11ef17fb727cfc36c8ab1d3
Katherine Kelly <ktkelly11@gmail.com>
2024-06-25 18:23:02 -0400

    More styling done with Homepage

--- src/components/homePageInfo/HomePageInfo.jsx
+++ src/components/homePageInfo/HomePageInfo.jsx
@@ -8,11 +8,18 @@ export default function HomePageInfo() {
       <div className="home-page-summary">
         <div className="site-summary">
           <p>
-            Thank you for visiting our site. Here at Secondhand Instruments, we
-            strive to provide every child access to the life-changing experience
-            of making music. We appreciate your interest in donating instruments
-            that will go to a child in a public school who is excited to play
-            and learn.
+            We believe that music can be transformative, and a musical
+            instrument can help a child discover not only their own musical
+            abilities but the joy of making music with others. Many children
+            love music and want to learn to play it. But many families simply
+            can’t afford the cost to rent or buy an instrument.
+            <br />
+            <br />
+            Here’s how you can help. Many families own gently used band and
+            orchestra instruments which are frequently gathering dust in a
+            closet or attic. We use volunteer labor and cash donations to
+            refurbish the instruments and work with teachers to get them to
+            students who otherwise would not have one.
           </p>
         </div>
         <div className="student-image">
