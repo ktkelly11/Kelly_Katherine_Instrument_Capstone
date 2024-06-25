ef9e1b3fd86db6162e5fce5ab3ea93fb06527597
Katherine Kelly <ktkelly11@gmail.com>
2024-06-25 18:44:38 -0400

    Styling updates

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,12 +6,13 @@ export default function Footer() {
       <div className="footer">
         <p>You can follow us here:</p>
         <div className="buttons">
-          <button className="github">
-            <a href="https://github.com/ktkelly11">Github</a>
-          </button>
-          <button className="linkedin">
-            <a href="www.linkedin.com/in/katherine-kelly11">Linkedin</a>
-          </button>
+          <a className="github" href="https://github.com/ktkelly11">
+            Github
+          </a>
+
+          <a className="linkedin" href="www.linkedin.com/in/katherine-kelly11">
+            Linkedin
+          </a>
         </div>
       </div>
     </>
