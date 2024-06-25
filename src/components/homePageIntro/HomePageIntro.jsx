12dd8ec86d33039a5ab68b136b0d782ba1ef0815
Katherine Kelly <ktkelly11@gmail.com>
2024-06-25 18:05:28 -0400

    Styling changes to navbar

--- src/components/homePageIntro/HomePageIntro.jsx
+++ src/components/homePageIntro/HomePageIntro.jsx
@@ -1,18 +1,11 @@
 import "./HomePageIntro.css";
-import music_intro from "../../images/homePage/music_intro.png";
 
 export default function HomePageIntro() {
   return (
     <>
       <div className="home-page-intro">
-        <div className="img">
-          <img src={music_intro} alt="" />
-        </div>
         <div className="title">
-          <h1>Play</h1>
-          <h1>It</h1>
-          <br />
-          <h1>Forward</h1>
+          <h1>Play It Forward</h1>
         </div>
       </div>
     </>
