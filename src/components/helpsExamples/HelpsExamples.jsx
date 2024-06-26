4a84be5bb1025e1262089bd8b603eb904493c933
Katherine Kelly <ktkelly11@gmail.com>
2024-06-25 21:28:34 -0400

    Helps updates

--- src/components/helpsExamples/HelpsExamples.jsx
+++ src/components/helpsExamples/HelpsExamples.jsx
@@ -3,26 +3,39 @@ import music_class from "../../images/howItHelps/music_class.webp";
 import music_class_2 from "../../images/howItHelps/music_class_2.jpg";
 import music_class_3 from "../../images/howItHelps/music_class_3.jpg";
 import music_class_4 from "../../images/howItHelps/music_class_4.jpg";
+import kindergarten_1 from "../../images/howItHelps/kindergarten_1.png";
+import students_4 from "../../images/howItHelps/students_4.png";
 
 export default function HelpExamples() {
   return (
     <>
-      <div className="teacher-1">
-        <img src={music_class_2} alt="Mrs. Singh teaching a class" />
+      <div className="helps-1">
+        <img src={kindergarten_1} alt="Music class" />
         <p>Explanation of why the instruments are important</p>
       </div>
-      <div className="teacher-2">
+      <div className="helps-2">
         <p>Explanation of why the instruments are important</p>
-        <img src={music_class} alt="Mr. Davis teaching a class" />
+        <img src={music_class_2} alt="Mr. Davis teaching a class" />
       </div>
-      <div className="teacher-3">
+      <div className="helps-3">
+        <img
+          src={music_class}
+          alt="Sam talks about what it means to him to be able to create music with the help of Ms. Johnson"
+        />
+        <p>Explanation of why the instruments are important</p>
+      </div>
+      <div className="helps-4">
+        <p>Explanation of why the instruments are important</p>
+        <img src={students_4} alt="Mr. Davis teaching a class" />
+      </div>
+      <div className="helps-5">
         <img
           src={music_class_3}
           alt="Sam talks about what it means to him to be able to create music with the help of Ms. Johnson"
         />
         <p>Explanation of why the instruments are important</p>
       </div>
-      <div className="teacher-4">
+      <div className="helps-6">
         <p>Explanation of why the instruments are important</p>
         <img src={music_class_4} alt="Mr. Davis teaching a class" />
       </div>
--- src/pages/DonationsPage.jsx
+++ src/pages/DonationsPage.jsx
@@ -1,13 +1,38 @@
+import { useState, useEffect } from "react";
 import NavBar from "../components/navBar/NavBar";
 import DonationsIntro from "../components/donationsIntro/DonationsIntro";
 import NewDonation from "../components/newDonation/NewDonation";
 
 function Donations() {
+  const [donations, setDonations] = useState([]);
+
+  const baseUrl = "https://instrument-capstone-backend.onrender.com";
+
+  useEffect(() => {
+    const getDonations = async () => {
+      try {
+        const response = await fetch(`${baseUrl}/donations`);
+
+        if (response.status !== 200) {
+          return;
+        }
+
+        const data = await response.json();
+
+        setDonations(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getDonations();
+  }, []);
+
   return (
     <>
       <NavBar />
       <DonationsIntro />
-      <NewDonation />
+      <NewDonation donations={donations} baseUrl={baseUrl} />
     </>
   );
 }
