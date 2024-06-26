31491fd30cc4adf72c2522544594a68da472b4ef
Katherine Kelly <ktkelly11@gmail.com>
2024-06-25 21:44:31 -0400

    Final Helps page edits

--- src/components/helpsExamples/HelpsExamples.jsx
+++ src/components/helpsExamples/HelpsExamples.jsx
@@ -28,26 +28,37 @@ export default function HelpExamples() {
         <img src={music_class_2} alt="Music class" />
       </div>
       <div className="helps-3">
-        <img
-          src={music_class}
-          alt="Sam talks about what it means to him to be able to create music with the help of Ms. Johnson"
-        />
-        <p>Explanation of why the instruments are important</p>
+        <img src={music_class} alt="Music class" />
+        <p>
+          Music has been shown to improve focus and attention and boost mental
+          performance when studying or when working on a task.
+        </p>
       </div>
       <div className="helps-4">
-        <p>Explanation of why the instruments are important</p>
-        <img src={students_4} alt="Mr. Davis teaching a class" />
+        <p>
+          Music can transport us to the past and can allow us to recall fond
+          memories. Listening creates connections in our brains to emotions we
+          experienced listening to that music in the past. Music has a profound
+          influence on the memories of those with dementia and Alzheimer’s.
+        </p>
+        <img src={students_4} alt="Music class" />
       </div>
       <div className="helps-5">
-        <img
-          src={music_class_3}
-          alt="Sam talks about what it means to him to be able to create music with the help of Ms. Johnson"
-        />
-        <p>Explanation of why the instruments are important</p>
+        <img src={music_class_3} alt="Music class" />
+        <p>
+          Listening to peaceful music can help with relaxation by slowing your
+          breathing and heart rate as well reducing blood pressure and reducing
+          stress hormones.
+        </p>
       </div>
       <div className="helps-6">
-        <p>Explanation of why the instruments are important</p>
-        <img src={music_class_4} alt="Mr. Davis teaching a class" />
+        <p>
+          Music brings people together from all walks of life. It doesn’t matter
+          what language you speak or where you are from, music is a language
+          that all of us understand. It brings people together at concerts,
+          celebrations and places of worship.
+        </p>
+        <img src={music_class_4} alt="Music class" />
       </div>
     </>
   );
