379e3850c47f8e1d9c4cc6a5ef00593fc9573761
Katherine Kelly <ktkelly11@gmail.com>
2024-06-25 22:25:59 -0400

    Updating CSS from index.css

--- src/components/donationsIntro/DonationsIntro.jsx
+++ src/components/donationsIntro/DonationsIntro.jsx
@@ -2,9 +2,9 @@ import "./DonationsIntro.css";
 
 export default function () {
   return (
-    <>
+    <div className="donations" style={{ textAlign: "center" }}>
       <h1 className="donations-title">Donations</h1>
-      <h2>Explanation of donations page:</h2>
-    </>
+      <h2 className="donations-explanation">Explanation of donations page:</h2>
+    </div>
   );
 }
