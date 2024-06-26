861a70c3376dcbbf18bc36088bbd231c399e39b0
Katherine Kelly <ktkelly11@gmail.com>
2024-06-25 22:52:27 -0400

    Updating Homepage after getting rid of Vite styling

--- src/components/donationsIntro/DonationsIntro.jsx
+++ src/components/donationsIntro/DonationsIntro.jsx
@@ -4,7 +4,20 @@ export default function () {
   return (
     <div className="donations" style={{ textAlign: "center" }}>
       <h1 className="donations-title">Donations</h1>
-      <h2 className="donations-explanation">Explanation of donations page:</h2>
+      <div className="donations-explanation">
+        <p>
+          We welcome donations of band and orchestra instruments, including
+          strings, brass, woodwinds, percussion and guitars. We do not accept
+          pianos and organs. Simply fill out our form and we will contact you.
+        </p>
+        <br />
+        <p>
+          Almost all instruments need some repair so they are in the best,
+          playable condition when schools receive them, so don’t worry if your
+          instrument needs adjustment, tuning or minor repairs. Our partners
+          repair our donated instruments at a greatly reduced cost.
+        </p>
+      </div>
     </div>
   );
 }
